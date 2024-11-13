11c714b8905228fa8a1b5dba876d30f3d4d788ea
Eden BETE <edenbete@Edens-MacBook-Air.local>
2024-11-13 20:05:23 +0100

    debug2

--- app.R
+++ app.R
@@ -1,39 +1,40 @@
 # Load necessary libraries
-install.packages('shinydashboard')  # Uncomment this if you haven't installed the package yet # nolint
+install.packages('shinydashboard')  # Uncomment this if you haven't installed the package yet
 library(shiny)
 library(shinydashboard)
 
 # Load the page
-source("pages/Home.R")  # Ensure this path is correct and points to your page1.R file # nolint
+source("pages/Home.R") 
 
 # User interface
 ui <- dashboardPage(
   dashboardHeader(title = "Immunization_Surveillance"),
-
+  
   # Sidebar navigation
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Home", tabName = "Home", icon = icon("dashboard"))
-     
-
-      
+      menuItem("Home", tabName = "Home", icon = icon("dashboard")),
+      menuItem("Filtres", tabName = "Filtres", icon = icon("dashboard"))
+      # Remove other menu items for now
     )
   ),
-
+  
   # Inside the dashboardBody() in app.R
   dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
     ),
     tabItems(
-      home_ui  # Reference the Home UI
+      Home_ui # Reference the Home UI
+      
     )
   )
 )
 
 # Server logic
 server <- function(input, output, session) {
-  callModule(home_server, "Home")  # Call only the server for Page 1
+  callModule(Home_server, "Home")
+    # Call only the server for Page 1
 }
 
 # Launch the application
